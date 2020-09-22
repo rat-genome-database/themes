ef4783ace7a553fa89293b50add57ab6c156be3b
jt15 <jthota01@gmail.com>
2020-09-22 11:52:38 -0500

    added vv link

--- rgd/header.php
+++ rgd/header.php
@@ -498,6 +498,7 @@
                                         <a href="/jbrowse/">JBrowse (Genome Browser)</a>
                                         <a href="/rgdweb/enrichment/start.html">Multi-Ontology Enrichment (MOET)</a>
                                         <a href="/rgdweb/ortholog/start.html">Gene-Ortholog Location Finder (GOLF)</a>
+                                        <a href="/rgdweb/vv/config.html"><span style="color:darkblue">Elastic Variant Visualizer</span>&nbsp;&nbsp;&nbsp;<span style="font-weight: bold;color:red">new</span></a>
                                         <a href="/rgdweb/front/config.html">Variant Visualizer</a>
                                         <a href="/rgdweb/cytoscape/query.html">InterViewer (Protein-Protein Interactions)</a>
                                         <a href="/rgdweb/phenominer/home.jsp">PhenoMiner (Quantitative Phenotypes)</a>
