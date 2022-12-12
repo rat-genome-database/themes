d0cf822f8f38148ec7c4ac713b533e8bfc0f1075
De Pons <jdepons@bme-ccn-16511.mcwcorp.net>
2022-12-12 15:49:29 -0600

    updated

--- rgd/header.php
+++ rgd/header.php
@@ -516,8 +516,6 @@
                                             <a href="https://www.alliancegenome.org/bluegenes/alliancemine">AllianceMine</a>
                                             <a href="/rgdweb/gTool/Gviewer.jsp">GViewer (Genome Viewer)</a>
                                             <a href="/rgdweb/overgo/find.html">Overgo Probe Designer</a>
-<%--                                            <a href="/ACPHAPLOTYPER/">ACP Haplotyper</a>--%>
-<%--                                            <a href="/GENOMESCANNER/">Genome Scanner</a>--%>
                                         </div>
                                 </div>
                                 <div class="rgd-dropdown">
